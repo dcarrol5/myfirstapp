abd7314432727be63b35a2ef39d20a4cd449711e
h22121 <h22121@lab.cpcc.edu>
2019-03-07 20:55:25 -0500

    2018-03-07

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-$(document).ready(function (){
+$(document).ready(function () {
     console.log("You are reading this from the console");
+
+    $("h1").css("color", "red");
+
+
+    paper.install(window);
+
+    paper.setup($("#mainCanvas")[0]);
+    var c = Shape.Circle(200, 200, 50);
+
+    c.fillColor = "green";
+
+    var tool = new Tool();
+
+    tool.onMouseDown = function (event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+
+        c.fillColor = "green";
+    }
+    /*
+       var c= Shape.Circle(200,200,50);
+    
+       c.fillcolor ="green";
+    */
+    paper.view.draw();
 });
